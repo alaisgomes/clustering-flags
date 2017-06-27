5eb94c2b6d6497f42c091382156d857e6b8151c1
Aline Tavares <alaisgomes@gmail.com>
2017-06-27 09:29:05 -0300

    tests ok

--- src/test_consistency.py
+++ src/test_consistency.py
@@ -3,75 +3,64 @@
 # Projeto - Agrupamento Espectral
 
 import csv
-import pytest
 
 data_file = "data/flag.csv"
 
-class InconsistencyException(Exception):
-    def __init__(self, value, country):
-        self.value = value
-        self.country = country
+# class InconsistencyException(Exception):
+#     def __init__(self, value, country):
+#         self.value = value
+#         self.country = country
 
 
-def test_landmass(land):
-    if int(land) not in range(1,7):
-        print(land)
+def test_int_in_range(value, myrange, test_type):
+    if int(value) not in myrange:
         raise ValueError
 
-def test_zone(zone):
-    if int(zone) not in range(1,5):
+def test_is_int(value, test_type):
+    if not isinstance(int(value), int):
         raise ValueError
 
-def test_area(area):
-    if not isinstance(int(area), int):
-        raise ValueError
-
-def test_population(pop):
-    if not isinstance(int(pop), int):
-        raise ValueError
-
-def test_language(lang):
-    if int(lang) not in range(1,11):
-        raise ValueError
-
-def test_religion(reli):
-    if int(reli) not in range(0,8):
-        raise ValueError
-
-def test_bars(bars):
-    if not isinstance(int(bars), int):
-        raise ValueError
-
-def test_stripes(stripes):
-    if not isinstance(int(stripes), int):
-        raise ValueError
-
-def test_stripes(stripes):
-    if not isinstance(int(stripes), int):
-        raise ValueError
-
+def test_bool(color, type):
+    if int(color) not in range(0,2):
+        raise ValueError 
 
 def main():
-    try:
-        with open(data_file, 'r') as csvfile:
-            flags_reader = csv.reader(csvfile, delimiter=',')
+    with open(data_file, 'r') as csvfile:
+        flags_reader = csv.reader(csvfile, delimiter=',')
 
-            for flag in flags_reader:
-                print (flag)
-                test_landmass(flag[1])
-                test_zone(flag[2])
-                test_area(flag[3])
-                test_population(flag[4])
-                test_language(flag[5])
-                test_religion(flag[6])
-                test_bars(flag[7])
+        for flag in flags_reader:
+            print (flag[0])
+            test_int_in_range(flag[1], range(1,7), "landmass")
+            test_int_in_range(flag[2], range(1,5), "zone")
+            test_is_int(flag[3], "area")
+            test_is_int(flag[4], "population")
+            test_int_in_range(flag[5], range(1,11), "language")
+            test_int_in_range(flag[6], range(0,8), "religion")
+            test_is_int(flag[7], "total_bars")
+            test_is_int(flag[8], "total_stripes")
+            test_is_int(flag[9], "total_colors")
 
+            for i in range(10,17):
+                test_bool(flag[i], "color_present")
 
-        csvfile.close()
-    
-    except:
-        print("Inconsistent")
-            
+            # 17 = mainhue
+
+            test_is_int(flag[18], "total_circles")
+            test_is_int(flag[19], "total_crosses")
+            test_is_int(flag[20], "total_saltires")
+            test_is_int(flag[21], "total_quarters")
+            test_is_int(flag[22], "total_sunstars")
+
+            test_bool(flag[23], "crescent_moon")
+            test_bool(flag[24], "triangle")
+            test_bool(flag[25], "icon")
+            test_bool(flag[26], "animate")
+            test_bool(flag[27], "text")
+
+            # 28: topleft color - name
+            # 29 : bottom right color - name
+
+    csvfile.close()            
 
 if __name__ == '__main__':
     main()
