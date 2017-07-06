c9d7c01774fbe08a5a08f0da8c12800a048c38f6
Joao Paulo Araujo <jpaulo.caraujo@gmail.com>
2017-07-05 22:18:33 -0300

    Utilizando matriz simetrica

--- src/matrix.py
+++ src/matrix.py
@@ -7,13 +7,18 @@ import scipy.linalg
 import matplotlib.pyplot as plt
 from scipy.cluster.vq import * #kmeans,vq, whiten
 import pylab
+from scipy import linalg
 
 # Constantes para modificar e obter resultados
 SIGMA = 13.0
 K_CONST = 10   
 ATTRIBUTES_RANGE = range(1,2)
 
+<<<<<<< Updated upstream
 final_file = "data/formatted.csv"
+=======
+SIGMA = 200
+>>>>>>> Stashed changes
 N_INSTANCES = sum(1 for line in open(final_file))
 
 
@@ -28,8 +33,15 @@ def initialize_matrix(size=N_INSTANCES):
 def euclidean_ditance(s1, s2):
     value = 0.0
 
+<<<<<<< Updated upstream
     for i in ATTRIBUTES_RANGE:
         value += math.pow((int(s1[i]) - int(s2[i])), 2)
+=======
+    #for i in range(4,6):
+     #   value += math.pow((int(s1[i]) - int(s2[i])), 2)
+    value += math.pow((int(s1[3]) - int(s2[3])), 2)
+    value += math.pow((int(s1[4]) - int(s2[4])), 2)
+>>>>>>> Stashed changes
 
     return math.sqrt(value)
 
@@ -68,9 +80,11 @@ def laplacian_matrix(A):
 
     np.fill_diagonal(D, A.sum(axis=1))
 
+    sqrD=linalg.sqrtm(D)
+
     for i in range(N_INSTANCES):
         for j in range(N_INSTANCES):
-            L[i, j] = D[i, j] - A[i, j]
+            L = sqrD * A *sqrD
     return L   
 
 def eigen_vectors(L):
